a181687b5612883139154a965cb9340ed61c6d7f
nadar_rossano <nadar@sogo.co.il>
2024-05-19 19:06:18 +0300

    fixing image size

--- FrontEnd/src/pages/Favorites.tsx
+++ FrontEnd/src/pages/Favorites.tsx
@@ -45,9 +45,9 @@ function Favorites() {
                 <h2 className='p-4 text-black dark:text-white'>
                   {app.artistName}
                 </h2>
-                <div className='w-[200px] h-full mx-auto overflow-hidden cursor-pointer pb-8'>
+                <div className='w-[150px] h-full mx-auto overflow-hidden cursor-pointer pb-8'>
                   <img
-                    className='p-5 w-[200px] object-cover rounded-[10px]'
+                    className='p-5 w-[150px] object-cover rounded-[10px]'
                     src={app.artworkUrl100}
                     alt={app.artistName}
                   />
